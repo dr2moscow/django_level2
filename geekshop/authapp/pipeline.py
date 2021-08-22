2ba81106521f456b8bf66ff93279c9de418f7ca2
dr2moscow <dr2moscow@gmail.com>
2021-08-22 17:27:20 +0300

    ДЗ 2 > 4. Проверить работу исключения «AuthForbidden», например, задав при проверке минимальный возраст 100 лет. Не содаем профиль, если юзер моложе 18 и старше 100 лет

--- geekshop/authapp/pipeline.py
+++ geekshop/authapp/pipeline.py
@@ -1,6 +1,6 @@
 from collections import OrderedDict
 from datetime import datetime
-from urllib.parse import urlencode, urlparse, urlunparse
+from urllib.parse import urlencode, urlunparse
 
 import requests
 from django.utils import timezone
@@ -27,5 +27,12 @@ def save_user_profile(backend, user, response, *args, **kwargs):
 
     if data['about']:
         user.shopuserprofile.about_me = data['about']
+
+    if data['bdate']:
+            bdate = datetime.strptime(data['bdate'], '%d.%m.%Y').date()
+            age = timezone.now().date().year - bdate.year
+            if age < 18 or age > 100:
+                user.delete()
+                raise AuthForbidden('social_core.backends.vk.VKOAuth2')
 
     user.save()
