fbf56d495147ad1ac56af3bbec53c4639946940b
dr2moscow <dr2moscow@gmail.com>
2021-08-29 18:53:16 +0300

    ДЗ 4

--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
+from django.db.models.signals import pre_save, pre_delete
+from django.dispatch import receiver
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy, reverse
@@ -37,6 +39,8 @@ class OrderItemsCreate(LoginRequiredMixin, CreateView):
                 for num, form in enumerate(formset.forms):
                     form.initial['product'] = basket_items[num].product
                     form.initial['quantity'] = basket_items[num].quantity
+                    form.initial['price'] = basket_items[num].product.price
+                    # basket_items[num].delete()
                 basket_items.delete()
             else:
                 formset = OrderFormSet()
@@ -65,6 +69,7 @@ class OrderItemsCreate(LoginRequiredMixin, CreateView):
 class OrderUpdate(LoginRequiredMixin, UpdateView):
     model = Order
     fields = []
+    # context_object_name = 'object'
     success_url = reverse_lazy('order:orders_list')
 
     def get_context_data(self, **kwargs):
@@ -72,13 +77,16 @@ class OrderUpdate(LoginRequiredMixin, UpdateView):
         OrderFormSet = inlineformset_factory(Order, OrderItem, form=OrderItemForm, extra=1)
 
         if self.request.POST:
-            formset = OrderFormSet(self.request.POST, instance=self.object)
+            data['orderitems'] = OrderFormSet(self.request.POST)
         else:
             formset = OrderFormSet(instance=self.object)
+            for form in formset.forms:
+                if form.instance.pk:
+                    form.initial['price'] = form.instance.product.price
 
             data['orderitems'] = formset
 
-            return data
+        return data
 
     def form_valid(self, form):
         context = self.get_context_data()
@@ -90,13 +98,15 @@ class OrderUpdate(LoginRequiredMixin, UpdateView):
             if orderitems.is_valid():
                 orderitems.instance = self.object
                 orderitems.save()
+
+        # удаляем пустой заказ
         if self.object.get_total_cost() == 0:
             self.object.delete()
 
         return super(OrderUpdate, self).form_valid(form)
 
 
-class OrderDelete(LoginRequiredMixin, DeleteView):
+class OrderDelete(LoginRequiredMixin,DeleteView):
     model = Order
     success_url = reverse_lazy('order:orders_list')
 
@@ -115,4 +125,23 @@ def order_forming_complete(request, pk):
     order = get_object_or_404(Order, pk=pk)
     order.status = Order.SENT_TO_PROCEED
     order.save()
+
     return HttpResponseRedirect(reverse('order:orders_list'))
+
+
+@receiver(pre_save, sender=OrderItem)
+@receiver(pre_save, sender=Basket)
+def product_quantity_update_save(sender, update_fields, instance, **kwargs):
+    if update_fields == 'quantity' or 'product':
+        if instance.pk:
+            instance.product.quantity -= instance.quantity - sender.get_item(instance.pk).quantity
+        else:
+            instance.product.quantity -= instance.quantity
+        instance.product.save()
+
+
+@receiver(pre_delete, sender=OrderItem)
+@receiver(pre_delete, sender=Basket)
+def product_quantity_update_delete(sender, instance, **kwargs):
+    instance.product.quantity += instance.quantity
+    instance.product.save()
